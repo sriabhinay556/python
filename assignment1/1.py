f7657b28d172c6a4187600c85554aaf41b754734
sriabhinay556 <69256623+sriabhinay556@users.noreply.github.com>
2020-08-05 21:43:45 +0530

    Add files via upload

--- assignment1/1.py
+++ assignment1/1.py
@@ -1,1 +1,28 @@
-print("hi")
+In [2]:
+year=int(input())
+if year%12==0:
+  print("monkey")
+elif year%12==1:
+  print("Rooster")
+elif year%12==2:
+  print("Dog")  
+elif year%12==3:
+  print("Pig")  
+elif year%12==4:
+  print("rat")  
+elif year%12==5:
+  print("ox")  
+elif year%12==6:
+  print("tiger")  
+elif year%12==7:
+  print("rabbit")  
+elif year%12==8:
+  print("dragon")  
+elif year%12==9:
+  print("snake")  
+elif year%12==10:
+  print("horse")  
+else:
+  print("sheep")
+2
+Dog
